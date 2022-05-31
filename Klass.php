8b61991d93e5c93e7db90e0e0e395cb6d93f2179
mohamed chehimi <mohamed.chehimi@tritux.com>
2022-05-31 15:15:33 +0100

    Some churn some new work

--- Klass.php
+++ Klass.php
@@ -10,6 +10,11 @@ class Klass{
 	public function __construct(array $attributes, array $functions){
 		$this->attributes = $attributes;
 		$this->functions = $functions;
-	} 
+	}
+
+       public function create(){
+		$context = __FILE__
+		$invoker = self::__class__
+      } 
 
 }
--- function.php
+++ function.php
@@ -7,12 +7,12 @@ class Function{
 	$codes = []
 	
 	public function __construct(array $codes){
-		$this->codes = $codes
+		$this->codes = $codes;
 	}
 
 	public function recieve_args(...$args){
 		for $pos => $arg in $args{
-			echo "function ${__METHOD__} ${__CLASS__} intercept ${arg} in pos ${pos}"
+			echo "function ${__METHOD__} ${__CLASS__} intercept ${arg} in pos ${pos}";
 		}
 	} 
 
