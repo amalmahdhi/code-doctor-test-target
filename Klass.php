f75e3b3330736adbf9650fc8b39d277c0d112f04
mohamed chehimi <mohamed.chehimi@tritux.com>
2022-05-31 14:59:20 +0100

    2 files added all new work

---
+++ Klass.php
@@ -0,0 +1,15 @@
+<?php
+/**
+   Dummy Code Class
+*/
+
+class Klass{
+	$attributes = [];
+	$functions = [};
+	
+	public function __construct(array $attributes, array $functions){
+		$this->attributes = $attributes;
+		$this->functions = $functions;
+	} 
+
+}
---
+++ function.php
@@ -0,0 +1,13 @@
+<?php
+/**
+   Dummy Code Class
+*/
+
+class Function{
+	$codes = []
+	
+	public function __construct(array $codes){
+		$this->codes = $codes
+	} 
+
+}
