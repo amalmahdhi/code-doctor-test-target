9be716cf56b6bdd11cc6081ac9a290c1cc5779f2
amalmahdhi <amal.mahdhi@tritux.com>
2022-08-23 14:36:45 +0100

    Nouveau Fichier

---
+++ NouveauFichier.php
@@ -0,0 +1,19 @@
+<?php
+/**
+   Dummy Code Class
+*/
+
+class Code{
+ 
+ private $content = '';
+ private $lang = '';
+ private $GLOBALS ='';
+ public $argc = '';
+
+ public function __construct($lang="php", $content="<?php"){
+   $this->lang = $lang;
+   $this->content = $content;
+
+ }
+
+}
