92a2f55238b50d3f9751a0d7525e53c19edb4d44
amalmahdhi <amal.mahdhi@tritux.com>
2022-08-23 14:39:54 +0100

    New work avec context lines

--- NouveauFichier.php
+++ NouveauFichier.php
@@ -8,6 +8,8 @@ class Code{
  private $content = '';
  private $lang = '';
  private $GLOBALS ='';
+ private $lang = '';
+ private $GLOBALS ='';
  public $argc = '';
 
  public function __construct($lang="php", $content="<?php"){
